7bde56e884f47ecde5b6a8db402aecdaadf234c8
clicube <clicube@gmail.com>
2019-02-15 00:04:33 +0900

    パスコード認証の処理とテスト

--- functions/api/routes/auth.js
+++ functions/api/routes/auth.js
@@ -1,21 +1,45 @@
 'use strict'
 const auth = require('basic-auth')
 module.exports = (config) => {
+  const checkBasicAuth = (req) => {
+    const user = auth(req)
+    if (user) {
+      return user && user.name === config.name && user.pass === config.pass
+    } else {
+      return undefined
+    }
+  }
+
+  const checkPasscode = (req) => {
+    if (req.body.passcode) {
+      return req.body.passcode === config.passcode
+    } else {
+      return undefined
+    }
+  }
+
+  const fail = (res) => {
+    return res.status(401)
+      .send({ result: 'NG', message: 'Unauthorized' })
+  }
+
   return {
     basicAuth: (req, res, next) => {
-      const user = auth(req)
-      if (user && user.name === config.name && user.pass === config.pass) {
+      if (checkBasicAuth(req)) {
         return next()
       } else {
-        return res.status(401)
-          .send({ result: 'NG', message: 'Unauthorized' })
+        return fail(res)
       }
     },
     passcodeOrBasicAuth: (req, res, next) => {
-      if (req.body.passcode === config.passcode) {
-        return next()
+      const basicAuthResult = checkBasicAuth(req)
+      const passcodeResult = checkPasscode(req)
+      if (!basicAuthResult && !passcodeResult) {
+        return fail(res)
+      } else if (basicAuthResult === false || passcodeResult === false) {
+        return fail(res)
       } else {
-        return this.basicAuth(req, res, next)
+        return next()
       }
     }
   }
--- functions/api/routes/auth.test.js
+++ functions/api/routes/auth.test.js
@@ -3,21 +3,36 @@
 const auth = require('./auth')
 const request = require('supertest')
 const express = require('express')
+const bodyParser = require('body-parser')
 
-const createTestApp = () => {
+const createTestApp = (testAuth) => {
   const app = express()
-  const config = { name: 'name', pass: 'pass' }
-  const sut = auth(config)
-  app.use(sut.basicAuth)
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(testAuth)
   app.get('/', (req, res) => {
+    res.send('ok')
+  })
+  app.post('/', (req, res) => {
     res.send('ok')
   })
   return app
 }
 
-test('basicAuth accept correct name and pass', async () => {
+const createTestAppBasic = () => {
+  const config = { name: 'name', pass: 'pass' }
+  const sut = auth(config)
+  return createTestApp(sut.basicAuth)
+}
+
+const createTestAppPassOrBasic = () => {
+  const config = { name: 'name', pass: 'pass', passcode: '1111' }
+  const sut = auth(config)
+  return createTestApp(sut.passcodeOrBasicAuth)
+}
+
+test('basicAuth accepts correct name and pass', async () => {
   // arrange
-  const app = createTestApp()
+  const app = createTestAppBasic()
 
   // act
   const result = await request(app).get('/').auth('name', 'pass')
@@ -29,7 +44,7 @@ test('basicAuth accept correct name and pass', async () => {
 
 test('basicAuth rejects wrong name', async () => {
   // arrange
-  const app = createTestApp()
+  const app = createTestAppBasic()
 
   // act
   const result = await request(app).get('/').auth('wrong-name', 'pass')
@@ -40,7 +55,7 @@ test('basicAuth rejects wrong name', async () => {
 
 test('basicAuth rejects wrong pass', async () => {
   // arrange
-  const app = createTestApp()
+  const app = createTestAppBasic()
 
   // act
   const result = await request(app).get('/').auth('name', 'wrong-pass')
@@ -48,3 +63,85 @@ test('basicAuth rejects wrong pass', async () => {
   // assert
   expect(result.status).toBe(401)
 })
+
+test('basicAuth rejects without basic auth header', async () => {
+  // arrange
+  const app = createTestAppBasic()
+
+  // act
+  const result = await request(app).get('/')
+
+  // assert
+  expect(result.status).toBe(401)
+})
+
+test('passcodeOrBasicAuth accepts correct passcode without basic auth',
+  async () => {
+    // arrange
+    const app = createTestAppPassOrBasic()
+
+    // act
+    const result = await request(app).post('/').send('passcode=1111')
+
+    // assert
+    expect(result.status).toBe(200)
+    expect(result.text).toBe('ok')
+  })
+
+test('passcodeOrBasicAuth accepts correct basic auth without passcode',
+  async () => {
+    // arrange
+    const app = createTestAppPassOrBasic()
+
+    // act
+    const result = await request(app).post('/').auth('name', 'pass')
+
+    // assert
+    expect(result.status).toBe(200)
+    expect(result.text).toBe('ok')
+  })
+
+test('passcodeOrBasicAuth rejects correct passcode with wrong basic auth',
+  async () => {
+    // arrange
+    const app = createTestAppPassOrBasic()
+
+    // act
+    const result = await request(app)
+      .post('/')
+      .send('passcode=1111')
+      .auth('name', 'wrong-pass')
+
+    // assert
+    expect(result.status).toBe(401)
+  })
+
+test('passcodeOrBasicAuth rejects correct basic auth with wrong passcode',
+  async () => {
+    // arrange
+    const app = createTestAppPassOrBasic()
+
+    // act
+    const result = await request(app)
+      .post('/')
+      .send('passcode=2222')
+      .auth('name', 'pass')
+
+    // assert
+    expect(result.status).toBe(401)
+  })
+
+test('passcodeOrBasicAuth rejects without basic auth and passcode',
+  async () => {
+    // arrange
+    const app = createTestAppPassOrBasic()
+
+    // act
+    const result = await request(app)
+      .post('/')
+      .send('passcode=2222')
+      .auth('name', 'pass')
+
+    // assert
+    expect(result.status).toBe(401)
+  })
